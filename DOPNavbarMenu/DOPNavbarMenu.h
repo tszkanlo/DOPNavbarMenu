61759c884076011e9d90a354efe7c4af59b2b679
Zachary <zacharylo@oursky.com>
2016-12-27 22:41:18 +0800

    Add tableview header offset support for USThing

--- DOPNavbarMenu/DOPNavbarMenu.h
+++ DOPNavbarMenu/DOPNavbarMenu.h
@@ -7,7 +7,49 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <UIKit/UIGestureRecognizerSubclass.h>
+#import "UIView+DOPExtension.h"
 
+@interface UITouchGestureRecognizer : UIGestureRecognizer
+@end
+
+@interface DOPNavbarMenuItem : NSObject
+
+@property (copy, nonatomic, readonly) NSString *title;
+@property (strong, nonatomic, readonly) UIImage *icon;
+
+- (instancetype)initWithTitle:(NSString *)title icon:(UIImage *)icon;
++ (DOPNavbarMenuItem *)ItemWithTitle:(NSString *)title icon:(UIImage *)icon;
+
+@end
+
+@class DOPNavbarMenu;
+@protocol DOPNavbarMenuDelegate <NSObject>
+
+- (void)didShowMenu:(DOPNavbarMenu *)menu;
+- (void)didDismissMenu:(DOPNavbarMenu *)menu;
+- (void)willDismissMenu:(DOPNavbarMenu *)menu;
+- (void)didSelectedMenu:(DOPNavbarMenu *)menu atIndex:(NSInteger)index;
+
+@end
+
+//iOS7+
 @interface DOPNavbarMenu : UIView
+
+@property (copy, nonatomic, readonly) NSArray *items;
+@property (assign, nonatomic, readonly) NSInteger maximumNumberInRow;
+@property (assign, nonatomic, getter=isOpen) BOOL open;
+@property (weak, nonatomic) id <DOPNavbarMenuDelegate> delegate;
+
+@property (strong, nonatomic) UIView *background;
+@property (strong, nonatomic) UIColor *textColor;
+@property (strong, nonatomic) UIColor *separatarColor;
+
+- (instancetype)initWithItems:(NSArray *)items
+                        width:(CGFloat)width
+           maximumNumberInRow:(NSInteger)max;
+
+- (void)showInNavigationController:(UINavigationController *)nvc tableView:(UITableView *)tableView;
+- (void)dismissWithAnimation:(BOOL)animation tableView:(UITableView *)tableView;
 
 @end
--- DOPNavbarMenu/DOPNavbarMenu.m
+++ DOPNavbarMenu/DOPNavbarMenu.m
@@ -8,14 +8,162 @@
 
 #import "DOPNavbarMenu.h"
 
+@implementation UITouchGestureRecognizer
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    self.state = UIGestureRecognizerStateRecognized;
+}
+
+@end
+
+@implementation DOPNavbarMenuItem
+
+- (instancetype)initWithTitle:(NSString *)title icon:(UIImage *)icon {
+    self = [super init];
+    if (self == nil) return nil;
+    _title = title;
+    _icon = icon;
+    return self;
+}
+
++ (DOPNavbarMenuItem *)ItemWithTitle:(NSString *)title icon:(UIImage *)icon {
+    return [[self alloc] initWithTitle:title icon:icon];
+}
+
+@end
+
+static NSInteger rowHeight = 100;
+static CGFloat titleFontSize = 15.0;
+
+@interface DOPNavbarMenu ()
+
+@property (assign, nonatomic) CGRect beforeAnimationFrame;
+@property (assign, nonatomic) CGRect afterAnimationFrame;
+@property (assign, nonatomic) NSInteger numberOfRow;
+
+@end
+
 @implementation DOPNavbarMenu
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithItems:(NSArray *)items width:(CGFloat)width maximumNumberInRow:(NSInteger)max {
+    self = [super initWithFrame:CGRectMake(0, 0, width, 0)];
+    if (self == nil) return nil;
+    _items = items;
+    _open = NO;
+    _maximumNumberInRow = max;
+    _numberOfRow = (_items.count-1)/_maximumNumberInRow + 1;
+    self.dop_height = (_numberOfRow+1) * rowHeight;
+    self.dop_y = -self.dop_height;
+    _beforeAnimationFrame = self.frame;
+    _afterAnimationFrame = self.frame;
+    self.backgroundColor = [UIColor whiteColor];
+    _background = [[UIView alloc] initWithFrame:CGRectZero];
+    _background.backgroundColor = [UIColor clearColor];
+    UITouchGestureRecognizer *gr = [[UITouchGestureRecognizer alloc] initWithTarget:self action:@selector(dismissMenu)];
+    //[_background addGestureRecognizer:gr];
+    _textColor = [UIColor whiteColor];
+    _separatarColor = [UIColor colorWithWhite:0.0 alpha:0.8];
+    return self;
 }
-*/
 
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    CGFloat buttonWidth = self.dop_width/self.maximumNumberInRow;
+    CGFloat buttonHeight = rowHeight;
+    [self.items enumerateObjectsUsingBlock:^(DOPNavbarMenuItem *obj, NSUInteger idx, BOOL *stop) {
+        CGFloat buttonX = (idx % self.maximumNumberInRow) * buttonWidth;
+        CGFloat buttonY = ((idx / self.maximumNumberInRow)+1) * buttonHeight;
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.accessibilityLabel = obj.title; //for users of voiceOver
+        button.frame = CGRectMake(buttonX, buttonY, buttonWidth, buttonHeight);
+        button.tag = idx;
+        [self addSubview:button];
+        [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+        UIImageView *icon = [[UIImageView alloc] initWithImage:obj.icon];
+        icon.center = CGPointMake(buttonWidth/2, buttonHeight/2-15);
+        [button addSubview:icon];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, buttonHeight-35, buttonWidth, 20)];
+        label.text = obj.title;
+        label.textAlignment = NSTextAlignmentCenter;
+        label.textColor = self.textColor;
+        label.font = [UIFont systemFontOfSize:titleFontSize];
+        [button addSubview:label];
+        if ((idx+1)%self.maximumNumberInRow != 0) {
+            UIView *separatar = [[UIView alloc] initWithFrame:CGRectMake(buttonWidth-0.5, 0, 0.5, buttonHeight)];
+            separatar.backgroundColor = self.separatarColor;
+            [button addSubview:separatar];
+        }
+        if (self.numberOfRow > 1 && idx/self.maximumNumberInRow < (self.numberOfRow-1)) {
+            UIView *separatar = [[UIView alloc] initWithFrame:CGRectMake(0, buttonHeight-0.5, buttonWidth, 0.5)];
+            separatar.backgroundColor = self.separatarColor;
+            [button addSubview:separatar];
+        }
+    }];
+}
+
+- (void)showInNavigationController:(UINavigationController *)nvc tableView:(UITableView *)tableView {
+    [nvc.view insertSubview:self.background belowSubview:nvc.navigationBar];
+    [nvc.view insertSubview:self belowSubview:nvc.navigationBar];
+    if (CGRectEqualToRect(self.beforeAnimationFrame, self.afterAnimationFrame)) {
+        CGRect tmp = self.afterAnimationFrame;
+        tmp.origin.y += ([UIApplication sharedApplication].statusBarFrame.size.height+nvc.navigationBar.dop_height+rowHeight*self.numberOfRow);
+        self.afterAnimationFrame = tmp;
+    }
+    self.background.frame = nvc.view.frame;
+    [tableView beginUpdates];
+    CGRect headerFrame = tableView.tableHeaderView.bounds;
+    headerFrame.size.height = 80;
+    [UIView animateWithDuration:0.5 delay:0.0
+                        options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                        self.dop_y = self.afterAnimationFrame.origin.y;
+                         tableView.tableHeaderView.bounds = headerFrame;
+                         [tableView endUpdates];
+                     } completion:^(BOOL finished) {
+                        if (self.delegate != nil) {
+                            [self.delegate didShowMenu:self];
+                        }
+                        self.open = YES;
+    }];
+}
+
+- (void)dismissWithAnimation:(BOOL)animation tableView:(UITableView *)tableView {
+    void (^completion)(void) = ^void(void) {
+        if (self.delegate != nil) {
+            [self.delegate willDismissMenu:self];
+        }
+        [self removeFromSuperview];
+        [self.background removeFromSuperview];
+        if (self.delegate != nil) {
+            [self.delegate didDismissMenu:self];
+        }
+        self.open = NO;
+    };
+    if (animation) {
+        [tableView beginUpdates];
+        [UIView animateWithDuration:0.5 animations:^{
+            self.dop_y = self.beforeAnimationFrame.origin.y;
+            tableView.tableHeaderView = [UIView new];
+            [tableView endUpdates];
+        } completion:^(BOOL finished) {
+            completion();
+        }];
+    } else {
+        self.dop_y = self.beforeAnimationFrame.origin.y;
+        tableView.tableHeaderView = [UIView new];
+        completion();
+    }
+}
+
+- (void)dismissMenu {
+    [self dismissWithAnimation:YES tableView:[[UITableView alloc]init]];
+}
+
+- (void)buttonTapped:(UIButton *)button {
+    if (self.delegate) {
+        [self.delegate didSelectedMenu:self atIndex:button.tag];
+    }
+    [self dismissMenu];
+}
 @end
